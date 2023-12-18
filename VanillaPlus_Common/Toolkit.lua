8be4411243533ba00f325e5768430fa65af35240
winnowplus <winnowplus@gmail.com>
2023-12-19 02:00:54 +0800

    add function InformNewBattlefieldInstances

--- VanillaPlus_Common/Toolkit.lua
+++ VanillaPlus_Common/Toolkit.lua
@@ -3,49 +3,47 @@
 local Namespace = VanillaPlus;
 local GetLogger = Namespace.GetLogger;
 
+local GetBattlefieldInfo = GetBattlefieldInfo;
+local GetNumBattlefields = GetNumBattlefields;
+local GetBattlefieldInstanceInfo = GetBattlefieldInstanceInfo;
+
 -----------------------------------------------  Declarations  ------------------------------------------------
 
-local BATTLEFIELD_ZONE_NAME_REGEX   = "(.+)%s(%d+)";
-local Logger                        = GetLogger();
+local Logger = GetLogger();
 
 -------------------------------------------------  Functions  -------------------------------------------------
 
 -- turtle specific
-function Namespace.ShowTurtleWowBattlefieldFrame(twBattlefieldIndex)
-    if(BattlefieldFrame:IsShown()) then
-        return;
-    end
+function Namespace.ShowTurtleWowBattlefieldFrame(twBattlefieldIndex, twBattlefieldName)
+    if(not BattlefieldFrame:IsShown() or twBattlefieldName ~= GetBattlefieldInfo()) then
+        local dropDownListButton = _G["DropDownList1Button" .. tostring(twBattlefieldIndex)];
 
-    local button = _G["DropDownList1Button" .. tostring(twBattlefieldIndex)];
-
-    if(DropDownList1:IsShown()) then
-        button:Click();
-    else
-        TWMiniMapBattlefieldFrame:Click();
-        button:Click();
+        if(DropDownList1:IsShown()) then
+            dropDownListButton:Click();
+        else
+            TWMiniMapBattlefieldFrame:Click();
+            dropDownListButton:Click();
+        end
     end
 end
 
-function Namespace.InformNewBattlefieldZoneIndex()
-    if(not BattlefieldFrame:IsShown()) then
-        return;
-    end
+function Namespace.InformNewBattlefieldInstances(twBattlefieldName)
+    if(BattlefieldFrame:IsShown() and twBattlefieldName == GetBattlefieldInfo()) then
+        local offset = 0;
+        local instances = {};
+        local numInstances = GetNumBattlefields();
 
-    local expectIndex = 1;
-    local zone = _G["BattlefieldZone" .. tostring(expectIndex + 1)];
-
-    while(zone ~= nil) do
-        local _, _, _, indexStr = string.find(zone:GetText(), BATTLEFIELD_ZONE_NAME_REGEX);
-        local index = tonumber(indexStr);
-
-        if(index ~= expectIndex) then
-            break;
-        else
-            expectIndex = expectIndex + 1;
-            zone = _G["BattlefieldZone" .. tostring(expectIndex + 1)];
-        end
-    end
+        for instanceIndex=1, numInstances do
+            local instanceId = GetBattlefieldInstanceInfo(instanceIndex);
     
-    Logger:Info("New zone would be ", expectIndex);
-    return;
+            if(instanceId == nil or instanceId ~= instanceIndex + offset) then
+                table.insert(instances, instanceIndex + offset);
+                offset = offset + 1;
+            end
+        end
+
+        table.insert(instances, tostring(numInstances + offset + 1) .. "+");
+
+        Logger:Info("New Instance would be ", table.concat(instances, ", "));
+    end
 end

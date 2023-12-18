8316ed73890c429cebf89f7cf7636be1af2e376a
winnowplus <winnowplus@gmail.com>
2023-12-18 22:43:08 +0800

    add function InformNewBattlefieldZoneIndex

--- VanillaPlus_Common/Toolkit.lua
+++ VanillaPlus_Common/Toolkit.lua
@@ -1,9 +1,16 @@
 --------------------------------------------------  Imports  --------------------------------------------------
 
 local Namespace = VanillaPlus;
+local GetLogger = Namespace.GetLogger;
+
+-----------------------------------------------  Declarations  ------------------------------------------------
+
+local BATTLEFIELD_ZONE_NAME_REGEX   = "(.+)%s(%d+)";
+local Logger                        = GetLogger();
 
 -------------------------------------------------  Functions  -------------------------------------------------
 
+-- turtle specific
 function Namespace.ShowTurtleWowBattlefieldFrame(twBattlefieldIndex)
     if(BattlefieldFrame:IsShown()) then
         return;
@@ -17,4 +24,28 @@ function Namespace.ShowTurtleWowBattlefieldFrame(twBattlefieldIndex)
         TWMiniMapBattlefieldFrame:Click();
         button:Click();
     end
+end
+
+function Namespace.InformNewBattlefieldZoneIndex()
+    if(not BattlefieldFrame:IsShown()) then
+        return;
+    end
+
+    local expectIndex = 1;
+    local zone = _G["BattlefieldZone" .. tostring(expectIndex + 1)];
+
+    while(zone ~= nil) then
+        local _, _, _, indexStr = string.find(zone:GetText(), BATTLEFIELD_ZONE_NAME_REGEX);
+        local index = tonumber(indexStr);
+
+        if(index ~= expectIndex) then
+            break;
+        else
+            expectIndex = expectIndex + 1;
+            zone = _G["BattlefieldZone2" .. tostring(expectIndex + 1)];
+        end
+    end
+    
+    Logger:Info("New zone would be: ", expectIndex);
+    return;
 end

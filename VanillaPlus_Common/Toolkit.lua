3eff732f24921ede5c9f0a7c3caf5d7d354b65b3
winnowplus <winnowplus@gmail.com>
2023-12-19 10:18:46 +0800

    bug fix

--- VanillaPlus_Common/Toolkit.lua
+++ VanillaPlus_Common/Toolkit.lua
@@ -3,9 +3,9 @@
 local Namespace = VanillaPlus;
 local GetLogger = Namespace.GetLogger;
 
-local GetBattlefieldInfo = GetBattlefieldInfo;
 local GetNumBattlefields = GetNumBattlefields;
 local GetBattlefieldInstanceInfo = GetBattlefieldInstanceInfo;
+local GetBattlefieldInfo = GetBattlefieldInfo;
 
 -----------------------------------------------  Declarations  ------------------------------------------------
 
@@ -13,37 +13,59 @@ local Logger = GetLogger();
 
 -------------------------------------------------  Functions  -------------------------------------------------
 
--- turtle specific
-function Namespace.ShowTurtleWowBattlefieldFrame(twBattlefieldIndex, twBattlefieldName)
-    if(not BattlefieldFrame:IsShown() or twBattlefieldName ~= GetBattlefieldInfo()) then
-        local dropDownListButton = _G["DropDownList1Button" .. tostring(twBattlefieldIndex)];
+local function NameIncludes(actual, except)
+    return actual and except and string.find(actual, except) and true or false;
+end
 
-        if(DropDownList1:IsShown()) then
-            dropDownListButton:Click();
-        else
-            TWMiniMapBattlefieldFrame:Click();
-            dropDownListButton:Click();
-        end
+local function PrivateSelectTWBattlefield(twBattlefieldIndex, twBattlefieldName)
+    local dropDownListButton = _G["DropDownList1Button" .. tostring(twBattlefieldIndex)];
+
+    if(DropDownList1:IsShown() and dropDownListButton and NameIncludes(dropDownListButton:GetText(), twBattlefieldName)) then
+        dropDownListButton:Click();
+    else
+        TWMiniMapBattlefieldFrame:Click();
+        dropDownListButton:Click();
     end
 end
 
-function Namespace.InformNewBattlefieldInstances(twBattlefieldName)
-    if(BattlefieldFrame:IsShown() and twBattlefieldName == GetBattlefieldInfo()) then
-        local offset = 0;
-        local instances = {};
-        local numInstances = GetNumBattlefields();
+local function PrivateInformTWBattlefieldNewInstances()
+    local offset = 0;
+    local instances = {};
+    local numInstances = GetNumBattlefields();
 
-        for instanceIndex=1, numInstances do
-            local instanceId = GetBattlefieldInstanceInfo(instanceIndex);
-    
-            if(instanceId == nil or instanceId ~= instanceIndex + offset) then
-                table.insert(instances, instanceIndex + offset);
-                offset = offset + 1;
-            end
+    for instanceIndex = 1, numInstances do
+        local instanceId = GetBattlefieldInstanceInfo(instanceIndex);
+
+        if(instanceId ~= instanceIndex + offset) then
+            table.insert(instances, instanceIndex + offset);
+            offset = offset + 1;
         end
+    end
 
-        table.insert(instances, tostring(numInstances + offset + 1) .. "+");
+    table.insert(instances, tostring(numInstances + offset + 1) .. "+");
 
-        Logger:Info("New Instance would be ", table.concat(instances, ", "));
+    Logger:Info("New Instance would be ", table.concat(instances, ", "));
+end
+
+function Namespace.ShowTWBattlefield(twBattlefieldIndex, twBattlefieldNam, informNewInstances)
+    local currBattlefield = GetBattlefieldInfo();
+
+    if(not BattlefieldFrame:IsShown() or not NameIncludes(currBattlefield, twBattlefieldNam)) then
+        PrivateSelectTWBattlefield(twBattlefieldIndex, twBattlefieldNam);
+    elseif(informNewInstances) then
+        PrivateInformTWBattlefieldNewInstances();
+    end
+end
+
+function Namespace.JoinTWBattlefield(twBattlefieldIndex, twBattlefieldNam, informNewInstances)
+    local currBattlefield = GetBattlefieldInfo();
+
+    if(not BattlefieldFrame:IsShown() or not NameIncludes(currBattlefield, twBattlefieldNam)) then
+        PrivateSelectTWBattlefield(twBattlefieldIndex, twBattlefieldNam);
+    elseif(informNewInstances) then
+        PrivateInformTWBattlefieldNewInstances();
+        BattlefieldFrameJoinButton:Click();
+    else
+        BattlefieldFrameJoinButton:Click();
     end
 end

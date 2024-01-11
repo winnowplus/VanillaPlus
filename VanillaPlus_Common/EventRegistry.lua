5d7117407ce5f26c0b824acbc468003e4e2bc919
winnowplus <winnowplus@gmail.com>
2024-01-11 13:41:19 +0800

    create EventRegistry

---
+++ VanillaPlus_Common/EventRegistry.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------  Imports  --------------------------------------------------
+
+local Namespace				= VanillaPlus;
+local CreateAndInitFromMixin= Namespace.CreateAndInitFromMixin;
+
+-----------------------------------------------  Declarations  ------------------------------------------------
+
+local ListenerMixin		= {};
+local EventRegistry     = {};
+Namespace.EventRegistry = EventRegistry;
+
+-------------------------------------------------  Functions  -------------------------------------------------
+

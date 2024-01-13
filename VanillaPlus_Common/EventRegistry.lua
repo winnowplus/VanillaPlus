aabb4791335b6279b7ed0b6eba2983c2620abda3
winnowplus <winnowplus@gmail.com>
2024-01-13 21:39:00 +0800

    1.13.9

--- VanillaPlus_Common/EventRegistry.lua
+++ VanillaPlus_Common/EventRegistry.lua
@@ -22,6 +22,7 @@ function EventRegistryMixin:Init(eventFrame)
     local onEvent = function(...)
         DEFAULT_CHAT_FRAME:AddMessage(event);
         DEFAULT_CHAT_FRAME:AddMessage(type(arg));
+        DEFAULT_CHAT_FRAME:AddMessage(tostring(arg1));
         
         for key, value in pairs(arg) do
             DEFAULT_CHAT_FRAME:AddMessage(tostring(key) .. " - " .. tostring(value));

aa5dc95769b79b61d18a7e8ff102245ac3cff95a
winnowplus <winnowplus@gmail.com>
2024-02-14 11:38:24 +0800

    2.14

--- VanillaPlus_Common/SpellBook.lua
+++ VanillaPlus_Common/SpellBook.lua
@@ -9,17 +9,49 @@ local GetSpellName      = GetSpellName;
 
 -----------------------------------------------  Declarations  ------------------------------------------------
 
-local SPELL_NAME_REGEX  = "(.+)%((.+)%)";
+local SPELL_NAME_FORMAT	= "%s (" .. LEVEL .. " %d)";
+
+local SPELL_CACHE       = nil;
+local PET_SPELL_CACHE   = nil;
 
 -------------------------------------------------  Functions  -------------------------------------------------
 
-local function TestLearnedSpell()
-    Namespace.GetLogger():Info("LEARNED_SPELL_IN_TAB ", arg1);
+local function GetSpellCahce()
+    if(SPELL_CACHE ~= nil) then
+        return SPELL_CACHE;
+    end
+
+    SPELL_CACHE = {};
+
+    for tabIndex = GetNumSpellTabs(), 1, -1 do
+        local _, _, offset, numSpells = GetSpellTabInfo(tabIndex);
+
+        for spellSlot = offset + numSpells, offset + 1, -1 do
+            local spellName, spellRank = GetSpellName(spellSlot, BOOKTYPE_SPELL);
+
+            if(spellName ~= nil) then
+                local spell = {name = spellName, rank = spellRank, fullname = spellName .. "(" .. spellRank .. ")", slot = spellSlot, bookType = BOOKTYPE_SPELL};
+
+                SPELL_CACHE[fullname] = spell;
+                SPELL_CACHE[name] = SPELL_CACHE[name] or spell;
+            end
+        end
+    end
 end
 
-local function TestSpellChange()
-    Namespace.GetLogger():Info("SPELLS_CHANGED ", arg1);
+function Namespace.GetSpellSlot(fullname)
+    local spell = GetSpellCahce()[fullname];
+
+    if(spell ~= nil) then
+        return spell.slot, spell.bookType;
+    end
 end
 
-EventRegistry:RegisterFrameEventAndCallback("LEARNED_SPELL_IN_TAB", TestLearnedSpell);
-EventRegistry:RegisterFrameEventAndCallback("SPELLS_CHANGED", TestSpellChange);
+----------------------------------------------  Event Callbacks  ----------------------------------------------
+
+local function CleanSpellCahce()
+    SPELL_CACHE = nil;
+end
+
+EventRegistry:RegisterFrameEventAndCallback("LEARNED_SPELL_IN_TAB", CleanSpellCahce);
+--EventRegistry:RegisterFrameEventAndCallback("SPELLS_CHANGED", TestSpellChange);

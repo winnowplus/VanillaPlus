5760a7995e0ac4075bcec47a0bd25a93b188ef7e
winnowplus <winnowplus@gmail.com>
2024-03-08 13:43:10 +0800

    3.8

--- VanillaPlus_Common/Item.lua
+++ VanillaPlus_Common/Item.lua
@@ -14,6 +14,9 @@ local GetContainerItemCooldown  = GetContainerItemCooldown;
 local InventoryItemMixin        = {};
 local ContainerItemMixin        = {};
 
+local PLAYER_INVENTORY          = {};
+local PLAYER_INVENTORY_BY_NAME  = {};
+
 -------------------------------------------------  Functions  -------------------------------------------------
 
 function InventoryItemMixin:Init(unit, slot)
@@ -60,8 +63,13 @@ end
 
 ----------------------------------------------  Event Callbacks  ----------------------------------------------
 
+local function ON_UNIT_INVENTORY_CHANGED()
+    Namespace.GetLogger("VanillaPlus", 0):Debug("BAG_UPDATE ", arg1, " ", arg2);
+end
+
 local function ON_BAG_UPDATE()
     Namespace.GetLogger("VanillaPlus", 0):Debug("BAG_UPDATE ", arg1);
 end
 
+EventRegistry:RegisterFrameEventAndCallback("UNIT_INVENTORY_CHANGED", ON_UNIT_INVENTORY_CHANGED);
 EventRegistry:RegisterFrameEventAndCallback("BAG_UPDATE", ON_BAG_UPDATE);
